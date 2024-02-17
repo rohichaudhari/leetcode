4e0ecdb6a42a99d1d20ff685e7d785f9a5afac89
rohichaudhari <137138335+rohichaudhari@users.noreply.github.com>
2024-02-17 09:15:01 +0530

    17/2/2023

---
+++ program/p1.c
@@ -0,0 +1,13 @@
+// Input: num1 = 12, num2 = 5
+// Output: 17
+// Explanation: num1 is 12, num2 is 5, and their sum is 12 + 5 = 17, so 17 is returne
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int num1=12;
+    int num2=5;
+    int sum;
+    sum=num1+num2;
+    printf("sum=%d",sum);
+}
---
+++ program/p5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("enter n number:");
+    scanf("%d",&n);
+    if(n%2!=1)
+    {
+        printf("number is power of 2");
+    }
+    else
+    {
+        printf("number is not power of 2");
+    }
+    
+
+}
